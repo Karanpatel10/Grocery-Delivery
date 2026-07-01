f41197860a6b87d5f1e82d2d9ec28d9251cbc879
Karanpatel10 <kbp4451@gmail.com>
2026-06-30 17:55:58 -0700

    fix order route

--- BACKEND/Routes/orderRoutes.ts
+++ BACKEND/Routes/orderRoutes.ts
@@ -4,10 +4,10 @@ import { createOrder, getSingleOrder, getOrderLocation, getUserOrders, updateOrd
 const orderRouter=express.Router();
 
 orderRouter.post("/",createOrder);
-orderRouter.get("/",getUserOrders);
-orderRouter.get("/:id",getSingleOrder);
-orderRouter.put("/:id/status",updateOrderStatus);
 orderRouter.get("/all",getAllOrdersAdmin);
 orderRouter.get("/:id/location",getOrderLocation);
+orderRouter.get("/:id",getSingleOrder);
+orderRouter.put("/:id/status",updateOrderStatus);
+orderRouter.get("/",getUserOrders);
 
 export default orderRouter;
