36fea69dd15470b9658c62f268bea39b4d737d71
Karanpatel10 <kbp4451@gmail.com>
2026-06-30 19:08:46 -0700

    fix order route auth admin

--- BACKEND/Routes/orderRoutes.ts
+++ BACKEND/Routes/orderRoutes.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { createOrder, getSingleOrder, getOrderLocation, getUserOrders, updateOrderStatus,  getAllOrdersAdmin } from "../Controllers/orderController.js";
+import auth from "../middleware/auth.js";
+import admin from "../middleware/admin.js";
 
 const orderRouter=express.Router();
 
-orderRouter.post("/",createOrder);
-orderRouter.get("/all",getAllOrdersAdmin);
-orderRouter.get("/:id/location",getOrderLocation);
-orderRouter.get("/:id",getSingleOrder);
-orderRouter.put("/:id/status",updateOrderStatus);
-orderRouter.get("/",getUserOrders);
+orderRouter.post("/",auth,createOrder);
+orderRouter.get("/all",auth,admin,getAllOrdersAdmin);
+orderRouter.get("/:id/location",auth,getOrderLocation);
+orderRouter.get("/:id",auth,getSingleOrder);
+orderRouter.put("/:id/status",auth,admin,updateOrderStatus);
+orderRouter.get("/",auth,getUserOrders);
 
 export default orderRouter;
--- BACKEND/middleware/admin.ts
+++ BACKEND/middleware/admin.ts
@@ -3,7 +3,7 @@ import { prisma } from '../Config/prisma.js';
 
 const admin=async(req:Request,res:Response,next:NextFunction)=>{
     try{
-        const userId=req.user?.id;
+        const userId=req.user!.id;
         if(!userId){
             return res.status(401).json({message:'Unauthroized'});
         }
